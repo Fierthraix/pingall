6a78e17e8044d8078e7cf2627f4c5d3e60ab1301
Lucas Rooyakkers <lucas.rooyakkers@protonmail.com>
2021-07-23 22:07:13 -0400

    Added option to avoid resolving hostnames Also doesn't try to resolve hostnames if avahi-resolve not present.

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,8 @@ pub struct Opt {
     /// Interface to search.
     #[structopt[short = "i", long = "interface"]]
     pub interface: Option<String>,
+
+    /// Don't attempt to resolve hostnames.
+    #[structopt(short = "d", long = "dont-resolve")]
+    pub dont_resolve: bool,
 }
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,22 @@ use nix::sys::socket;
 use structopt::StructOpt;
 use tokio::process::Command;
 
+async fn command_exists(command: &str) -> bool {
+    let command = Command::new("which")
+        .arg(command)
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+        .expect("Error")
+        .wait()
+        .await;
+
+    match command {
+        Ok(status) => status.success(),
+        Err(_) => false,
+    }
+}
+
 fn get_addresses(interface: Option<String>) -> Vec<Ipv4Addr> {
     let filter_ip = |address| {
         if let Some(socket::SockAddr::Inet(inet_addr)) = address {
@@ -49,17 +65,23 @@ fn get_addresses(interface: Option<String>) -> Vec<Ipv4Addr> {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opt = Opt::from_args();
 
+    let resolve = !opt.dont_resolve && command_exists("avahi-resolve").await;
+
     let addresses = get_addresses(opt.interface);
 
     for address in addresses {
         let octets = address.octets();
-        run_subnet(&format!("{}.{}.{}.", octets[0], octets[1], octets[2])).await?;
+        run_subnet(
+            &format!("{}.{}.{}.", octets[0], octets[1], octets[2]),
+            resolve,
+        )
+        .await?;
     }
 
     Ok(())
 }
 
-async fn run_subnet(subnet: &str) -> Result<(), Box<dyn std::error::Error>> {
+async fn run_subnet(subnet: &str, resolve: bool) -> Result<(), Box<dyn std::error::Error>> {
     let mut pings = Vec::with_capacity(255);
     for i in 1..255 {
         let ip_addr = IpAddr::V4(format!("{}{}", subnet, i).parse().unwrap());
@@ -81,15 +103,20 @@ async fn run_subnet(subnet: &str) -> Result<(), Box<dyn std::error::Error>> {
             };
 
             if success {
-                let get_hostname = Command::new("avahi-resolve-address")
-                    .arg(ip_addr.to_string())
-                    .stderr(Stdio::null())
-                    .output();
+                if resolve {
+                    let get_hostname = Command::new("avahi-resolve")
+                        .arg("--address")
+                        .arg(ip_addr.to_string())
+                        .stderr(Stdio::null())
+                        .output();
 
-                let output = get_hostname.await.unwrap();
-                if output.status.success() && !output.stdout.is_empty() {
-                    let utf8_out = String::from_utf8_lossy(&output.stdout).trim().to_string();
-                    Some(utf8_out)
+                    let output = get_hostname.await.unwrap();
+                    if output.status.success() && !output.stdout.is_empty() {
+                        let utf8_out = String::from_utf8_lossy(&output.stdout).trim().to_string();
+                        Some(utf8_out)
+                    } else {
+                        Some(ip_addr.to_string())
+                    }
                 } else {
                     Some(ip_addr.to_string())
                 }
